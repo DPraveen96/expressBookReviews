d565fe60561e86def720daa06221b884e99d1e69
DPraveen96 <141646401+DPraveen96@users.noreply.github.com>
2023-09-09 23:54:41 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,19 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(300).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(300).json({message: "Unable to register user."});
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
